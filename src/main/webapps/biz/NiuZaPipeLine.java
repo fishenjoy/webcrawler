81a31ae4ee3d0657884bc7816d0beb3723f20fba
fishenjoy <fishenjoy@163.com>
2017-09-29 13:47:07 +0800

    将mybatis链接数据库内容配置化

--- src/main/webapps/biz/NiuZaPipeLine.java
+++ src/main/webapps/biz/NiuZaPipeLine.java
@@ -2,9 +2,12 @@ package biz;
 
 import dao.MySqlDao;
 import entity.NiuZa;
+import mapping.NiuZaMapperI;
+import org.apache.ibatis.session.SqlSession;
 import us.codecraft.webmagic.ResultItems;
 import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.pipeline.Pipeline;
+import utils.MybatisUtil;
 
 import java.util.List;
 
@@ -16,11 +19,15 @@ public class NiuZaPipeLine implements Pipeline{
     @Override
     public void process(ResultItems resultItems, Task task) {
         List<NiuZa> niuZaList=(List<NiuZa> )resultItems.get("niuza");
+        SqlSession sqlSession= MybatisUtil.getSqlSession(true);
+        NiuZaMapperI niuZaMapperI=sqlSession.getMapper(NiuZaMapperI.class);
         for (NiuZa niuZa:niuZaList) {
-            MySqlDao.insert(niuZa);
+            /*//替换为mybatis方式
+            MySqlDao.insert(niuZa);*/
+            niuZaMapperI.add(niuZa);
             i++;
         }
-
+        sqlSession.close();
 
     }
 }
--- src/test/java/MybatisTest.java
+++ src/test/java/MybatisTest.java
@@ -1,21 +1,41 @@
 import entity.NiuZa;
+import mapping.NiuZaMapperI;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import utils.MybatisUtil;
 
 import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Created by zzy on 2017/9/22.
  */
 public class MybatisTest {
     public static void main(String[] args){
-        String resource="mybatisConf.xml";
+        /*String resource="mybatisConf.xml";
         InputStream is=MybatisTest.class.getClassLoader().getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(is);
         SqlSession sqlSession=sqlSessionFactory.openSession();
         String statement="mapping.niuzaMapper.getNiuza";
         NiuZa niuZa=sqlSession.selectOne(statement,888);
+        System.out.println(niuZa.toString());*/
+
+
+        SqlSession sqlSession= MybatisUtil.getSqlSession(true);
+        NiuZaMapperI niuZaMapperI=sqlSession.getMapper(NiuZaMapperI.class);
+        //add
+//        NiuZa niuZa=new NiuZa();
+//        niuZa.setProductname("测试商品");
+//        niuZa.setInsertdate(new Date());
+//        niuZa.setPrice(9999);
+//        int add=niuZaMapperI.add(niuZa);
+//        sqlSession.close();
+//        System.out.println(add);
+
+//select
+        NiuZa niuZa=niuZaMapperI.getById(888);
+        sqlSession.close();
         System.out.println(niuZa.toString());
 
 
